bec1756d09c4b5dd71a294e80c298c12c359c53f
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2024-01-09 23:26:20 +0700

    Second commit

--- src/blocks/contacts/contacts.js
+++ src/blocks/contacts/contacts.js
@@ -1,14 +1,3 @@
-$('.about__slider').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  arrows: false,
-  swipe: true,
-  dots: true,
-  variableWidth: true,
-  autoplay: true,
-  autoplaySpeed: 2000
-});
-
 const inputName = document.querySelector('.contacts__field--name');
 const inputPhone = document.querySelector('.contacts__field--phone');
 const regSymbolName = /[0-9!@#$%^&*()+/_=]/g;
@@ -33,27 +22,4 @@ inputPhone.oninput = function () {
   };
 }
 
-
-
-var menu = $('.header__menu');
-var burgerBtn = $('.header__burger-btn');
-
-$(function () {
-    $(burgerBtn).on('click', function () {
-        $(this).toggleClass('header__burger-btn--open');
-        $(menu).toggleClass('header__menu--open');
-    });
-});
-
-
-$(function () {
-  $('.product').on('click', function () {
-    if ($(this).hasClass('product--visible')) {
-      $(this).removeClass('product--visible');
-    } else {
-      $('.product').removeClass('product--visible');
-      $(this).addClass('product--visible');
-    }
-  });
-});
 
